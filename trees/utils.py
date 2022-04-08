619400441870734f4cb24dba0ac26443ebb67852
Andreas Holck Høeg-Petersen <ahhp@cs.aau.dk>
2022-04-08 12:42:05 +0200

    remove old unused functions

--- trees/utils.py
+++ trees/utils.py
@@ -2,85 +2,19 @@ import pydot
 import csv
 import json
 import numpy as np
-import matplotlib.pyplot as plt
 from collections import defaultdict
-from sklearn.cluster import KMeans
 
 from trees.models import Node, Leaf, State
 
-def build_tree(tree, a, variables=None):
-    if isinstance(tree, float):
-        return Leaf(tree, action=a)
-
-    return Node(
-        tree['var'] if variables is None else variables[tree['var']],
-        tree['bound'],
-        low=build_tree(tree['low'], a, variables),
-        high=build_tree(tree['high'], a, variables)
-    )
-
-def make_state(tree, state, leafs):
-    if isinstance(tree, Leaf):
-        tree.state = state
-        leafs.append(tree)
-        return leafs
-
-    state_low = state.copy()
-    state_high = state.copy()
-
-    state_low.less_than(tree.variable, tree.bound)
-    leafs = make_state(tree.low, state_low, leafs)
-
-    state_high.greater_than(tree.variable, tree.bound)
-    return make_state(tree.high, state_high, leafs)
-
-def combine_leafs(l1, l2):
-    s1, s2 = l1.state, l2.state
-    variables = s1.variables
-
-    s = State(variables)
-    for var in variables:
-        s.less_than(var, min(s1.max[var], s2.max[var]))
-        s.greater_than(var, max(s1.min[var], s1.min[var]))
-
-    return (s, (l1, l2))
-
-def find_compatible(l1, ls):
-    out = []
-    for l2 in ls:
-        if l1.state.compatible(l2.state):
-            out.append(l2)
-
-    return out
-
-def plot_clusters(data, k=4):
-    fig, axs = plt.subplots(len(data), 1)
-    for ax, var in enumerate(data.keys()):
-        X = np.array(data[var]['bounds']).reshape(-1,1)
-        model = KMeans(n_clusters=k)
-        model.fit(X)
-
-        clusters = model.predict(X)
-        for c in range(k):
-            axs[ax].scatter(
-                X[clusters == c],
-                [10 for _ in range(len(X[clusters == c]))],
-                label=var,
-                marker='x'
-            )
-        axs[ax].get_yaxis().set_visible(False)
-        axs[ax].grid(axis='x')
-        axs[ax].set_title(var)
-    plt.tight_layout()
-    plt.show()
+####### Functions to add color gradients #######
 
 def hex_to_RGB(hex):
-  ''' "#FFFFFF" -> [255,255,255] '''
+  """ "#FFFFFF" -> [255,255,255] """
   # Pass 16 to the integer function for change of base
   return [int(hex[i:i+2], 16) for i in range(1,6,2)]
 
 def RGB_to_hex(RGB):
-  ''' [255,255,255] -> "#FFFFFF" '''
+  """ [255,255,255] -> "#FFFFFF" """
   # Components need to be integers for hex to make sense
   RGB = [int(x) for x in RGB]
   return "#"+"".join(["0{0:x}".format(v) if v < 16 else
@@ -92,6 +26,9 @@ def color_gradient(weight, start="#FFFFFF", end="#000000"):
     return RGB_to_hex([
         int(s[j] + (weight * (f[j] - s[j]))) for j in range(3)
     ])
+
+
+####### Functions to draw a tree and save it to png #######
 
 def _draw_graph(graph, tree, n, print_action=False):
     if isinstance(tree, Leaf):
@@ -144,6 +81,9 @@ def draw_graph(trees, labels=None, out_fp='graph_drawing.png', print_action=Fals
         n += 1
 
     graph.write_png(out_fp)
+
+
+####### Functions to load and add statistics to a tree #######
 
 def load_stat(fp):
     with open(fp, 'r') as f:
@@ -221,6 +161,20 @@ def add_stats(trees, stats, variables, max_ts, step_sz):
 
     return actions
 
+
+####### Function to load and build tree #######
+
+def build_tree(tree, a, variables=None):
+    if isinstance(tree, float):
+        return Leaf(tree, action=a)
+
+    return Node(
+        tree['var'] if variables is None else variables[tree['var']],
+        tree['bound'],
+        low=build_tree(tree['low'], a, variables),
+        high=build_tree(tree['high'], a, variables)
+    )
+
 def load_tree(fp, loc='(1)', stats=[]):
     with open(fp, 'r') as f:
         data = json.load(f)
@@ -239,108 +193,3 @@ def load_tree(fp, loc='(1)', stats=[]):
         add_stats(roots, stats)
 
     return roots, variables, actions
-
-def merge_state(s1, s2, maximize=True):
-    assert s1.variables == s2.variables
-    new_state = State(s1.variables)
-    for var in new_state.variables:
-        if maximize:
-            new_state.less_than(var, max(s1.max[var], s2.max[var]))
-            new_state.greater_than(var, min(s1.min[var], s2.min[var]))
-        else:
-            new_state.less_than(var, min(s1.max[var], s2.max[var]))
-            new_state.greater_than(var, max(s1.min[var], s2.min[var]))
-
-    return new_state
-
-def intersects(s1, s2):
-    assert s1.variables == s2.variables
-    variables = s1.variables
-
-    for var in variables:
-        if s1.min[var] < s2.max[var] < s1.max[var]:
-            return True
-
-        if s1.min[var] < s2.min[var] < s1.max[var]:
-            return True
-    return False
-
-def cut_leaf(l1, l2):
-    """
-    Cuts the intersecting part of `l2.state` out of `l1.state` and returns the
-    list of new leafs
-
-    We probably should build a tree within the space defined by the state of
-    `l1`. Make a root Node with state `l1.state` and then `.put` values from
-    `l2.state`
-    """
-    s1, s2 = l1.state, l2.state
-    if not intersects(s1, s2):
-        return []
-
-    variables = s1.variables
-
-    nodes = [Node(None, None, state=s1.copy())]
-    for var in variables:
-        if s1.min[var] < s2.min[var] < s1.max[var]:
-            nodes[len(nodes)-1].variable = var
-            nodes[len(nodes)-1].bound = s2.min[var]
-
-            new_state = s1.copy()
-            new_state.less_than(s2.min[var])
-            nodes[len(nodes)-1].low = Leaf(l1.cost, action=l1.action, state=new_state)
-
-        if s1.min[var] < s2.max[var] < s1.max[var]:
-            pass
-
-def prune_leafs(leafs):
-    """
-    Prunes all leafs in `leafs` that are dominated by a lower cost leaf
-
-    params:
-        leafs:  a list of `Leaf`s, sorted by their cost
-    """
-    leafs = leafs.copy()
-    start = 0
-    while start < len(leafs):
-        p = leafs[start]
-        for leaf in leafs[start+1:]:
-            if p.state.contains(leaf.state):
-                leafs.remove(leaf)
-
-        start += 1
-
-    return leafs
-
-def deep_pruning(leafs):
-    """
-    Supposed to go backward through a list of leafs sorted according to cost,
-    and check each leaf if its entirely covered by lower valued leafs. Expects
-    that for any two leafs, l_1 and l_2, it holds that if cost(l_1) < cost(l_2)
-    then l_2 is not covered by l_1.
-
-    However, it currently does not work
-    """
-
-    leafs = leafs.copy()
-
-    start = len(leafs) - 1
-    while start > 0:
-        p = leafs[start]
-        istate = None
-
-        for i in range(start - 1, -1, -1):
-            leaf = leafs[i]
-            if intersects(leaf.state, p.state):
-                if istate is None:
-                    istate = leaf.state.copy()
-                else:
-                    istate = merge_state(leaf.state, istate)
-
-            if istate is not None and istate.contains(p.state):
-                leafs.remove(p)
-                break
-
-        start -= 1
-
-    return leafs
