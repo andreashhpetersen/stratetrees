8c7b29c7ad1476965a644d293b9d019fbfc85525
Andreas Holck Høeg-Petersen <ahhp@cs.aau.dk>
2022-10-18 11:48:05 +0200

    add argument to evaluate.sh

--- automated/bouncing_ball/evaluate.sh
+++ automated/bouncing_ball/evaluate.sh
@@ -2,14 +2,14 @@
 
 file="./queries.q"
 V="~/uppaal-4.1.20-stratego-10-linux64/bin/verifyta"
+D=$1
 
-rm ./results.txt
 touch ./results.txt
-for f in ./constructed_0/* ; do
+for f in $D/* ; do
     Q=$(mktemp)
     echo "strategy s = loadStrategy {} -> {Ball(0).p, Ball(0).v} (\"$f\")" > $Q
     cat "./eval_queries.q" >> $Q
     CMD="$V model.xml $Q -s -W -q -y >> results.txt"
-    echo "$CMD"
+    echo "Evaluate $f"
     eval "$CMD"
 done
