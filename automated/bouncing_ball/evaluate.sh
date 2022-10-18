a49e37ec999ebdbc2732a40d6ccea36ac7d9a898
Andreas Holck Høeg-Petersen <ahhp@cs.aau.dk>
2022-10-18 10:19:06 +0200

    add bouncing ball experiment

---
+++ automated/bouncing_ball/evaluate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file="./queries.q"
+V="~/uppaal-4.1.20-stratego-10-linux64/bin/verifyta"
+
+rm ./results.txt
+touch ./results.txt
+for f in ./constructed_0/* ; do
+    Q=$(mktemp)
+    echo "strategy s = loadStrategy {} -> {Ball(0).p, Ball(0).v} (\"$f\")" > $Q
+    cat "./queries.q" >> $Q
+    CMD="$V model.xml $Q -s -W -q -y >> results.txt"
+    echo "$CMD"
+    eval "$CMD"
+done
---
+++ automated/bouncing_ball/queries.q
@@ -0,0 +1,1 @@
+E[<=120;1000] (max:LearnerPlayer.fired + (sum (id : ball_id) (Ball(id).number_deaths) * 1000 )) under s
