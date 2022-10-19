972735c727874cc4ebb09a8ee466570072a7fc30
Andreas Holck Høeg-Petersen <ahhp@cs.aau.dk>
2022-10-19 10:24:35 +0200

    update evaluate scripts

--- automated/traffic_light/evaluate.sh
+++ automated/traffic_light/evaluate.sh
@@ -9,6 +9,7 @@ fi
 V="~/uppaal-4.1.20-stratego-10-linux64/bin/verifyta"
 D=$1
 
+
 if [ -f ./results.txt ]
 then
     rm ./results.txt
@@ -16,8 +17,8 @@ fi
 touch ./results.txt
 for f in $D/* ; do
     Q=$(mktemp)
-    echo "strategy s = loadStrategy {} -> {Ball(0).p, Ball(0).v} (\"$f\")" > $Q
-    echo "E[<=120;1000] (max:LearnerPlayer.fired + (sum (id : ball_id) (Ball(id).number_deaths) * 1000 )) under s" >> $Q
+    echo "strategy s = loadStrategy {Light.location} -> {x, y, E, S} (\"$f\")" > $Q
+    echo "E[<=100;100] (max:Q) under s" >> $Q
     CMD="$V model.xml $Q -s -W -q -y >> results.txt"
     echo "Evaluate $f"
     eval "$CMD"
