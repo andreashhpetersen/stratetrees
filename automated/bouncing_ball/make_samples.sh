140640e728d85e518bad2257ff7f64ef35bff778
Peter G. Jensen <root@petergjoel.dk>
2022-11-09 11:37:14 +0100

    added log conversion tool

--- automated/bouncing_ball/make_samples.sh
+++ automated/bouncing_ball/make_samples.sh
@@ -6,6 +6,6 @@ M=./model.xml
 for s in $S ; do
     Q=$(mktemp)
     echo "strategy s = loadStrategy {} -> {Ball(0).p, Ball(0).v} (\"qt_strategy.json\")" > $Q
-    echo "simulate [<=300;$s] {Ball(0).p, Ball(0).v, LearnerPlayer.C}" >> $Q
+    echo "simulate [<=300;$s] {LearnerPlayer.C, Ball(0).p, Ball(0).v}" >> $Q
     $VERIFYTA_PATH $M $Q -sqyW > sample_${s}.log
 done
