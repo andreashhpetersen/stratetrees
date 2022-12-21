a21b3a3e7806c32ae485d103ff078ab22273ba26
Andreas Holck Høeg-Petersen <ahhp@cs.aau.dk>
2022-12-21 15:01:57 +0100

    fix error

--- automated/random_walk/make_eval_query.sh
+++ automated/random_walk/make_eval_query.sh
@@ -4,9 +4,9 @@ S=$1
 
 Q=$(mktemp)
 if [[ "$S" == *"qt_strategy.json" ]] ; then
-    echo "strategy s = loadStrategy {P.location} -> {t*1.0, d*1.0} (\"$S\")" > $Q
+    echo "strategy s = loadStrategy {P.location} -> {d*10.0, -t*10.0} (\"$S\")" > $Q
 else
-    echo "strategy s = loadStrategy {} -> {P.location, t*1.0, d*1.0} (\"$S\")" > $Q
+    echo "strategy s = loadStrategy {} -> {d*10.0, -t*10.0} (\"$S\")" > $Q
 fi
 echo "E[<=100;10000] (max:cost) under s" >> $Q
 echo $Q
