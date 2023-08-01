e7d3bb352533454c6a8ff8cc1336c664d67b71d7
Andreas Holck Høeg-Petersen <ahhp@cs.aau.dk>
2023-08-01 14:40:43 +0200

    update functions for building tree from leaves

--- trees/advanced.py
+++ trees/advanced.py
@@ -136,14 +136,13 @@ def init_bounds(partitions, K):
 
 
 def get_unexhausted_dim(exhausted):
-    # return np.arange(exhausted.shape[0])[exhausted == 0][0]
     return np.random.choice(np.arange(exhausted.shape[0])[exhausted == 0])
 
 
 def update_track_tree(track, state):
     leaf = make_leaf(1, track.variables, state)
     if track.root is None:
-        track.root = track.make_root_from_leaf(leaf)
+        track.root = Node.make_node_from_leaf(leaf, track.variables)
     else:
         track.put_leaf(leaf, prune=True)
 
@@ -158,32 +157,6 @@ def update_region_bounds(p_state, state, tree, lmap):
     parts = tree.predict_for_region(state[:,0], state[:,1], node_ids=True)
     for nid in parts:
         lmap[nid] = cut_overlaps(p_state, lmap[nid])
-
-
-def add_points(p_state, points, n_bounds):
-    pmin, pmax = p_state
-    K = len(pmin)
-    npoints = []
-
-    p_state = np.vstack(p_state).T
-    mesh = np.array(np.meshgrid(*p_state)).T.reshape(-1,K)
-    for p in mesh:
-        npoints.append(p)
-        heapq.heappush(points, tuple(p))
-
-    # for i in range(K):
-    #     # if pmax[i] == n_bounds[i]:
-    #     #     continue
-
-    #     new_point = pmin.copy()
-    #     new_point[i] = pmax[i]
-
-    #     npoints.append(new_point)
-    #     heapq.heappush(points, tuple(new_point))
-
-    # npoints.append(tuple(pmax))
-    # heapq.heappush(points, tuple(pmax))
-    return npoints
 
 
 def find_unexplored_state_dfs(n, state, func):
@@ -231,8 +204,6 @@ def max_parts3(tree, seed=None, return_info=False, \
 
     mstate = make_state(tuple((0,) * K for _ in range(2)), bounds)
     while True:
-        # if len(regions) % 100 == 0:
-        #     print(f'{len(regions)} regions, track size: {track.n_leaves}')
 
         if track.root is None:
             node_id = tree.predict_node_id(mstate[:,0], cheat=True)
@@ -328,16 +299,11 @@ def max_parts3(tree, seed=None, return_info=False, \
             bs = leaves_to_state_constraints(regions)[:,draw_dims,:]
             acts = [l.action for l in regions]
 
-            if mstate is not None and isinstance(bs, State):
+            if isinstance(mstate, State):
                 bs = np.vstack((bs, [mstate.constraints]))
                 acts.append(2)
 
             plot_voxels(bs, acts, max_v=max_v)
-
-    # if len(ts) > 0:
-    #     ts = np.array(ts)
-    #     print(f'avg track size: {ts.mean():0.2f} (+/- {ts.std():0.2f})')
-    #     print(f'max track size: {ts.max()}')
 
     if return_info:
         info = { 'track_sizes': np.array(ts) }
@@ -346,179 +312,10 @@ def max_parts3(tree, seed=None, return_info=False, \
     return regions
 
 
-### Functions for old max parts algorithm ###
-
-
-def grow(p, bs, max_i):
-    """
-    Non-deterministically choose an unexhausted dimension in which to increment
-    `p` by one.
-
-    p:  (K,) array of indicies
-    bs: (K, M) array of bounds, where M is the padded max number of bounds
-    max_i: (K,) array of max number of bounds for each dimension
-    """
-    idxs = np.arange(len(p))
-    np.random.shuffle(idxs)
-    i = idxs[np.logical_and(bs[idxs,-1] == 0, p[idxs] < max_i[idxs])][0]
-    p[i] += 1
-    return p, i
-
-
-def max_parts(tree, min_vals=None, max_vals=None, padding=1):
-    """
-    Args:
-        root: the root `Node` from which to extract boxes
-        variables: the list of variables in `root`
-        max_vals: (optional) a list of maximum values for the variables. If not
-                  provided, the maximum values will all be set to infinity
-        min_vals: (optional) a list of minimum values for the variables. If not
-                  provided, the minimum values will be calculated by subtracting
-                  1 from the smallest bound for each variable
-    """
-
-    # the number of state dimensions
-    K = len(tree.variables)
-
-    if min_vals is None:
-        min_vals = [None for _ in range(K)]
-
-    if max_vals is None:
-        max_vals = [None for _ in range(K)]
-
-    # each element in vbounds is a list of constraints for a specific dimension
-    vbounds = tree.get_bounds()  # assumed to be sorted in ascending order
-
-    # store the number of constraints for any dimension i (for indexing)
-    max_i = np.zeros((K,), dtype=int)
-
-    # build list of constraints
-    bounds = []
-    for i in range(K):
-        bs = np.array(vbounds[i])
-
-        # add min and max values if there are none
-        if min_vals[i] is None:
-            if bs.shape[0] == 0:
-                min_vals[i] = -math.inf
-            else:
-                min_vals[i] = np.amin(bs) - padding
-
-        if max_vals[i] is None:
-            if bs.shape[0] == 0:
-                max_vals[i] = math.inf
-            else:
-                max_vals[i] = np.amax(bs) + padding
-
-        # make list of constraints in range [min_val, max_val] (inclusive)
-        bs = np.hstack((
-            min_vals[i],
-            bs[np.logical_and(bs > min_vals[i], bs < max_vals[i])],
-            max_vals[i]
-        ))
-
-        # register number of constraints minus 1 (ie. the index of the last
-        # constraint)
-        max_i[i] = bs.shape[0] - 1
-
-        # add the array to the list of bounds
-        bounds.append(bs)
-
-    # arrange the constraints in a (K,M) matrix, where M is the length of the
-    # largest list of constraints + 1. The + 1 is for marking the dimension as
-    # exhausted
-    max_len = np.amax(max_i) + 2
-    for i in range(K):
-        bounds[i] = np.pad(bounds[i], (0, max_len - bounds[i].shape[0]))
-    bounds = np.vstack(bounds)
-
-    # start the list of points from the minimum values
-    points = [[0 for _ in range(K)]]
-
-    # we use a heap to process the points in a lexical order
-    heapq.heapify(points)
-
-    # define a tree to keep track of what has been covered
-    track = DecisionTree.empty_tree(tree.variables, tree.actions)
-
-    # this is what we return in the end - lets start!
-    regions = []
-
-    # we keep going for as long as there are points
-    while len(points) > 0:
-
-        # p_min and p_max contains indicies of the current constraints
-        p_min = np.array(heapq.heappop(points), dtype=int)
-        p_max = p_min.copy() + 1
-
-        # define the region spanned by min_state and max_state
-        min_state = bounds[np.arange(K), p_min]
-        max_state = bounds[np.arange(K), p_max]
-
-        # check if we have already explored this state
-        if is_explored(min_state, max_state, track):
-            continue
-
-        # get the action that the entire region must agree on
-        action = tree.predict(max_state)
-
-        # reset exhausted variables
-        bounds[:,-1] = 0
-        bounds[:,-1][p_max == max_i] = 1
-
-        while np.sum(bounds[:,-1]) < K:
-            # grow in dimension i and update p_max and max_state
-            p_max, i = grow(p_max, bounds, max_i)
-            max_state[i] = bounds[i][p_max[i]]
-
-            # state to express difference between previous max_state and current
-            diff_state = min_state.copy()
-            diff_state[i] = bounds[i][p_max[i] - 1]
-
-            # check if new region is explored
-            explored = is_explored(diff_state, max_state, track)
-
-            # check if our new region returns a more than one action
-            actions = tree.get_for_region(diff_state, max_state)
-
-            if (actions != set([action]) or explored or p_max[i] == max_i[i]):
-
-                # mark variable as exhausted
-                bounds[i,-1] = 1
-
-                # roll back to last state
-                if actions != set([action]) or explored:
-                    p_max[i] -= 1
-                    max_state[i] = diff_state[i]
-
-        # create the region as a leaf with a state spanned by min_state and
-        # max_state
-        state = State(tree.variables, np.vstack((min_state, max_state)).T)
-        leaf = Leaf(cost=0, action=action, state=state)
-        regions.append(leaf)
-
-        # create the root of the tracking tree if we haven't done so yet
-        if track.root is None:
-            track.root = track.make_root_from_leaf(leaf)
-        # or add to the tree, so we know not to explore this part again
-        else:
-            track.put_leaf(leaf)
-
-        # add new points from which to start later
-        for i in range(K):
-            if p_max[i] < max_i[i]:
-
-                new_p = [idx for idx in p_min]
-                new_p[i] = p_max[i]
-                heapq.heappush(points, new_p)
-
-    return regions
-
-
 ### Functions for reconstructing a DecisionTree from a list of leaves ###
 
 
-def split_box(leaf, v, c):
+def split_leaf(leaf, v, c):
     vmin, vmax = leaf.state.min_max(v)
 
     if vmin < c < vmax:
@@ -540,20 +337,20 @@ def split_box(leaf, v, c):
     return low, high
 
 
-def find_best_cut(boxes, variables, vmap):
+def split_leaves_list(leaves, variables):
 
-    # map each variable to a sorted list of indicies of the leaves in `boxes'
+    # map each variable to a sorted list of indicies of the leaves in `leaves'
     # where the sorting is done according to maximum value of the respective
     # variable for that leaf
     max_sorted = {
         # the variable is key, the list is the value
         v: sorted(
-            # this just generates indicies from [0, 1, ..., len(boxes) - 1]
-            range(len(boxes)),
+            # this just generates indicies from [0, 1, ..., len(leaves) - 1]
+            range(len(leaves)),
 
             # this sorts the above indicies in ascending order according to the
             # maximum value of v in the box that the index corresponds to
-            key=lambda x: list(boxes[x].state.min_max(v))[::-1]
+            key=lambda x: list(leaves[x].state.min_max(v))[::-1]
         )
 
         # iterate the variables
@@ -566,7 +363,7 @@ def find_best_cut(boxes, variables, vmap):
     # go through each variable
     for v in variables:
 
-        # list of indicies of entries in `boxes' sorted according to their max
+        # list of indicies of entries in `leaves' sorted according to their max
         # value for the variable `v'
         curr_l = max_sorted[v]
 
@@ -574,17 +371,17 @@ def find_best_cut(boxes, variables, vmap):
         for i in range(len(curr_l) - 1):
 
             # this is the lowest max value for v in the rest of the list (ie.
-            # boxes[curr_l[i]].state.max(v) <= boxes[curr_l[j]].state.max(v)
+            # leaves[curr_l[i]].state.max(v) <= leaves[curr_l[j]].state.max(v)
             # for all j > i)
-            max_v = boxes[curr_l[i]].state.max(v)
+            max_v = leaves[curr_l[i]].state.max(v)
 
-            if max_v == boxes[curr_l[i+1]].state.max(v):
+            if max_v == leaves[curr_l[i+1]].state.max(v):
                 continue
 
-            # ideally, we'd split at i == len(boxes) / 2, so we start by
+            # ideally, we'd split at i == len(leaves) / 2, so we start by
             # defining impurity as the difference between `i' and the optimal
             # value (the halfway point)
-            impurity = abs((len(boxes) / 2) - (i + 1))
+            impurity = abs((len(leaves) / 2) - (i + 1))
 
             # look at the rest of curr_l (with large max values for v)
             for j in range(i + 1, len(curr_l)):
@@ -592,62 +389,53 @@ def find_best_cut(boxes, variables, vmap):
                 # if the min value for v in curr_l[j] is less that max_v, we
                 # know that the box will be cut in 2, since it's max value for
                 # v by design must be greater than max_v
-                if boxes[curr_l[j]].state.min(v) < max_v:
+                if leaves[curr_l[j]].state.min(v) < max_v:
                     impurity += 1
 
             # add the triplet to our list of possible cuts
             cuts.append((v, i, impurity))
 
-    if len(cuts) == 0:
-        assert len(set([l.action for l in boxes]))
-        leaf = Leaf(0, action=boxes[0].action)
-        leaf.id = boxes[0].id
-        return leaf
-
     # sort according to impurity and take the 'most pure' cut
     v, b_id, _ = sorted(cuts, key=lambda x: x[2])[0]
 
     # grab that optimal value
-    max_val = boxes[max_sorted[v][b_id]].state.max(v)
+    max_val = leaves[max_sorted[v][b_id]].state.max(v)
 
-    # separate the boxes into list of those that are lower than our cut, those
+    # separate the leaves into list of those that are lower than our cut, those
     # that are higher or both if it falls on both sides of the cut
     low, high = [], []
-    for b in boxes:
-        l, h = split_box(b, v, max_val)
+    for b in leaves:
+        l, h = split_leaf(b, v, max_val)
         if l is not None:
             low.append(l)
         if h is not None:
             high.append(h)
 
     # something went wrong if we end up here
-    if len(low) == 0 or len(high) == 0:
-        import ipdb; ipdb.set_trace()
+    assert not (len(low) == 0 or len(high) == 0)
 
     # create the new branch node with a cut on v <= max_val
-    return (v, vmap[v], max_val), low, high
+    return (v, max_val), low, high
 
 
-def cut_to_node(boxes, variables, vmap):
-    if len(boxes) == 1:
-        leaf = Leaf(0.0, action=boxes[0].action)
-        return leaf
+def make_branch_node(leaves, variables, vmap):
 
-    res = find_best_cut(boxes, variables, vmap)
-    if isinstance(res, Leaf):
-        return res
+    # only one leaf, return it
+    if len(leaves) == 1:
+        return Leaf(0.0, action=leaves[0].action)
 
-    # else
-    (v, v_id, bound), low_ls, high_ls = res
+    # split on v <= bound
+    (v, bound), low_ls, high_ls = split_leaves_list(leaves, variables)
 
-    low = cut_to_node(low_ls, variables, vmap)
-    high = cut_to_node(high_ls, variables, vmap)
-    return Node(v, v_id, bound, low=low, high=high)
+    # make child nodes
+    low = make_branch_node(low_ls, variables, vmap)
+    high = make_branch_node(high_ls, variables, vmap)
+
+    # return branch node
+    return Node(v, vmap[v], bound, low=low, high=high)
 
 
-def boxes_to_tree(boxes, variables, actions=[]):
-    tree = DecisionTree.empty_tree(variables, actions)
-    root = cut_to_node(boxes, variables, tree.var2id).prune()
-    root.set_state(State(variables))
-    tree.root = root
-    return tree
+def leaves_to_tree(leaves, variables, actions=[]):
+    vmap = { v: i for i, v in enumerate(variables) }
+    root = make_branch_node(leaves, variables, vmap)
+    return DecisionTree(root.prune(), variables, actions)
